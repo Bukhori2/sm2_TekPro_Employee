587e0050672dd5ddeaca0897774f594bf0b2930e
Ahmad Bukhori <bukhori.fahri27@gmail.com>
2026-02-21 23:08:13 +0700

    Push semua code

---
+++ src/id/ac/polban/employee/Main.java
@@ -0,0 +1,24 @@
+package id.ac.polban.employee;
+
+import id.ac.polban.employee.model.*;
+import id.ac.polban.employee.service.EmployeeService;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeService service = new EmployeeService();
+
+        Department itDept = new Department("IT");
+        EmploymentType fullTime = new EmploymentType("Full-Time");
+
+        Employee emp1 = new Employee(1, "Rojak", itDept, fullTime, 5000000);
+        Employee emp2 = new Employee(2, "Ali", itDept, fullTime, 6000000);
+        service.addEmployee(emp1);
+        service.addEmployee(emp2);
+
+        System.out.println("Total Employee: " + Employee.getTotalEmployee());
+
+        System.out.println("Employee Name: " + service.getEmployee(1).getName());
+        service.raiseSalary(1, 30);
+        System.out.println("New Salary: " + service.getEmployee(1).getSalary());
+    }
+}
